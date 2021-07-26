e762b959e2d875409d5ce95f891ebd4a8bc1563b
edwinacevedo <edwinacevedog@gmail.com>
2021-07-26 17:10:56 -0500

    v0.0.2

---
+++ demo2.js
@@ -0,0 +1,25 @@
+/*
+function secondLargestNumber(numbers){
+    let first = numbers[0]
+    let second = 0
+    for (let i=0; i<numbers.length; i++ ){
+        if(numbers[i] > first){
+            second = first;
+            first = numbers[i]
+        }
+        if(numbers[i]>second && numbers[i]<first){
+            second = numbers[i]
+        }
+    }
+    return second
+}
+*/
+
+function secondLargestNumber2(numbers){
+    numbers.sort()
+    return numbers[1]
+}
+
+let nums = [4, 6, 30, 1, 62]
+console.log(secondLargestNumber2(nums))
+document.write(secondLargestNumber2(nums))
